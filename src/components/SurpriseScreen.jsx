02a7b1fb1d5131ad01b3da8cb1bc1be721b040ae
Reshmasudeepa <reshmasudeepa1@gmail.com>
2026-08-11 09:30:01 +0530

    22

--- src/components/SurpriseScreen.jsx
+++ src/components/SurpriseScreen.jsx
@@ -1,33 +1,18 @@
+import surpriseImage from "../assets/photos/image.png";
+
 function SurpriseScreen({ onOpen }) {
   return (
     <section className="surprise-screen">
-      <div className="floating-heart heart-1">♥</div>
-      <div className="floating-heart heart-2">♥</div>
-      <div className="floating-heart heart-3">♥</div>
-      <div className="floating-heart heart-4">♥</div>
+      <img
+        src={surpriseImage}
+        alt="Happy Birthday Sindhu"
+        className="surprise-cover"
+      />
 
-      <div className="surprise-card">
-        <div className="gift">🎁</div>
-
-        <p className="small-text">Hey, Sindhu 👀</p>
-
-        <h1>
-          I made something
-          <br />
-          special for you...
-        </h1>
-
-        <p className="surprise-description">
-          Because a normal birthday message was just not enough. 💗
-        </p>
-
+      <div className="surprise-cta">
         <button className="primary-btn" onClick={onOpen}>
           Open Your Surprise 🎁
         </button>
-
-        <span className="hint">
-          Best experienced with a smile ✨
-        </span>
       </div>
     </section>
   );
